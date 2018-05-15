8c281691aa85f728d355c2edb299b3a6b4806bdc
chris <chrisstevens901@gmail.com>
2018-05-15 21:53:31 +0100

    Data analysis and small temporary data set.

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
 
-train = pd.read_csv('train.csv')
+train = pd.read_csv('train_tmp.csv')
 
 print(train.head())
+print(train.axes)
+
+trainSubset = train[['price', 'deal_probability', 'item_seq_number']]
+
+numberOfStandardDeviationsToInclude = 0.5
+
+trainSubset[((trainSubset.price - trainSubset.price.mean()) / trainSubset.price.std()).abs() < numberOfStandardDeviationsToInclude]
+trainSubset[((trainSubset.deal_probability - trainSubset.deal_probability.mean()) / trainSubset.deal_probability.std()).abs() < numberOfStandardDeviationsToInclude]
+trainSubset[((trainSubset.item_seq_number - trainSubset.item_seq_number.mean()) / trainSubset.item_seq_number.std()).abs() < numberOfStandardDeviationsToInclude]
+
+pd.plotting.scatter_matrix(trainSubset, alpha = 0.3, figsize = (14,8), diagonal = 'kde')
+plt.show()
+print(trainSubset.head())
